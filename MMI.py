155ca851371b2831ff6e5a30fc3d000cb0a1301c
sinan-ozel <4183117+sinan-ozel@users.noreply.github.com>
2020-06-10 01:38:01 -0400

    Switch to numpy to make it scalable
    Should run faster now.

--- MMI.py
+++ MMI.py
@@ -3,6 +3,10 @@ import statistics
 import math
 import plotly
 
+import numpy as np
+from scipy.ndimage.interpolation import shift
+
+
 # Market Meanness Index
 def MMI(timeSeries_IN):
 	M = statistics.median(timeSeries_IN)
@@ -19,10 +23,18 @@ def MMI(timeSeries_IN):
 		P_prev = P
 	return 100*(th+td)/(l-1)
 
+def fastMMI(timeSeries_IN):
+	M = statistics.median(timeSeries_IN)
+	l = len(timeSeries_IN)
+	th = ((timeSeries_IN < M) & (timeSeries_IN > shift(timeSeries_IN, -1, cval=np.NaN))).sum()
+	td = ((timeSeries_IN > M) & (timeSeries_IN < shift(timeSeries_IN, -1, cval=np.NaN))).sum()
+	# See: https://stackoverflow.com/questions/30399534/shift-elements-in-a-numpy-array
+	return 100 * (th + td) / (l - 1)
+
 # Plots time series and prints MMI
 def MMI_Analyze(timeSeries_IN, logName_IN):
 	print("MMI "+logName_IN+": ", end=" ")
-	mmi = MMI(timeSeries_IN)
+	mmi = fastMMI(timeSeries_IN)
 	print(mmi)
 	fig = plotly.graph_objs.Figure()
 	fig.add_trace(plotly.graph_objs.Scatter(y=timeSeries_IN,
@@ -33,28 +45,31 @@ def MMI_Analyze(timeSeries_IN, logName_IN):
 
 def main():
 	l = 200 		# Sample size
-	random.seed(1)		# Fix samples
+	np.random.seed(1)		# Fix samples
 
 	# Gaussian, mean 0, variance 0.25
-	seriesGaussian = [random.gauss(0.0, 0.25) for i in range(l)]
+	seriesGaussian = np.random.normal(0.0, 0.25, l)
+	# See: https://numpy.org/doc/stable/reference/random/generated/numpy.random.Generator.normal.html#numpy.random.Generator.normal
 	MMI_Analyze(seriesGaussian, 'gaussian')
 	MMI_Analyze(sorted(seriesGaussian), 'sortedgaussian')
 
 	# Pure full cycle sin
-	seriesSin = [math.sin((i/l)*2.0*math.pi ) for i in range(l)]
+	seriesSin = np.sin(np.linspace(-np.pi, np.pi, l))
+	print(len(seriesSin))
+	# https://numpy.org/doc/1.18/reference/generated/numpy.sin.html
 	MMI_Analyze(seriesSin, 'sin')
 
 	# Noisy full cycle sin
-	seriesNoisySin = [seriesGaussian[i]+seriesSin[i] for i in range(l)]
-	MMI_Analyze(seriesNoisySin, 'noisysin')	
+	seriesNoisySin = seriesGaussian + seriesSin
+	MMI_Analyze(seriesNoisySin, 'noisysin')
 
 	# Pure exponential - 0.01% growth for 10 cycles and normalized by 8000
-	seriesExp = [math.exp(1.0001*i)/8000 for i in range(10)]
+	seriesExp = np.exp(np.linspace(1.0001, 1.0010, 10)) / 8000
 	MMI_Analyze(seriesExp, 'exp')
 
 	# Noisy exponential - 0.01% growth for 10 cycles and normalized by 8000
-	seriesNoisyExp = [seriesGaussian[i]+seriesExp[i] for i in range(10)]
-	MMI_Analyze(seriesNoisyExp, 'noisyexp')		
+	seriesNoisyExp = seriesGaussian[0:10] + seriesExp[0:10]
+	MMI_Analyze(seriesNoisyExp, 'noisyexp')
 
 if __name__ == "__main__":
     main()
